7eb8902b43be270d8d0e4ac75b1873cb31eaf540
John Best <john@digitaldownbeat.com>
2021-12-31 16:42:38 -0600

    working on Heroku deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static('/www'));
+app.use('/', express.static('www'));
 
 app.use('/collections', collectionsRouter);
 
